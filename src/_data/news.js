8aa6a611bcdd4d14f6363932c26450548bfbb176
Phil Hawksworth <phil@netlify.com>
2020-02-29 13:49:06 +0000

    descriptive env var

--- src/_data/news.js
+++ src/_data/news.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 module.exports = async function() {
   try {
-    const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.API_KEY}&pageSize=5`);
+    const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
     return response.data;
   } catch (error) {
     console.error(error);
